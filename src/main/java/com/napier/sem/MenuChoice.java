09cabe67ce0ed55bbc14ada762fc48ce46da2a9b
Colin <colin.edmiston@googlemail.com>
2021-02-03 13:28:52 +0000

    Working menu tested in app & docker

--- src/main/java/com/napier/sem/MenuChoice.java
+++ src/main/java/com/napier/sem/MenuChoice.java
@@ -2,5 +2,56 @@ package com.napier.sem;
 
 public class MenuChoice {
 
-    //main menu
+    //Function for root menu
+    public void mainMenu() {
+        Menu menu = new Menu();
+
+        menu.setTitle("*** Global Reports ***");
+        menu.addItem(new MenuItem("Countries", this, "countries"));
+        menu.addItem(new MenuItem("Cities" ,this,"cities"));
+        menu.addItem(new MenuItem("Capital Cities",this,"capitalCities"));
+        menu.addItem(new MenuItem("Population",this,"population"));
+        menu.execute();
+    }
+
+    //Countries menu choice
+    public void countries() {
+        Menu menu = new Menu();
+
+        menu.setTitle("*** Countries Reports ***");
+        menu.addItem(new MenuItem("All countries in world, largest first population",this,"allCountriesInWorld"));
+        menu.addItem(new MenuItem("All countries in a continent",this,"allCountriesInContinent"));
+        menu.execute();
+    }
+
+
+    public void cities() {
+
+    }
+
+    public void capitalCities() {
+
+
+    }
+
+    public void population() {
+
+
+    }
+
+    public void allCountriesInWorld() {
+
+
+    }
+
+    public void allCountriesInContinent() {
+
+        MenuSelection.continentChoice();
+    }
+
+
+
+
+
 }
+
