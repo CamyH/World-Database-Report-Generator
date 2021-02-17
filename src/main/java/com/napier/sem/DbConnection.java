85f5837a1810c08becc163e6c0d072b4157eba8b
Colin <colin.edmiston@googlemail.com>
2021-02-17 15:18:03 +0000

    Added getConnection() function

--- src/main/java/com/napier/sem/DbConnection.java
+++ src/main/java/com/napier/sem/DbConnection.java
@@ -3,7 +3,6 @@ package com.napier.sem;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.*;
 
 /* This class handles the connection and disconnection
 of the database.
