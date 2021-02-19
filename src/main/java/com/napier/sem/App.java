e261ba2b5cd3fca0b70f5671f8f897e8d102cf79
Cameron Hunt <cameronhunt88@hotmail.com>
2021-02-19 14:07:12 +0000

    Implemented a method to calculate the world population

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,6 @@
 package com.napier.sem;
 
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  * Project Authors: Colin, Cameron, Luke, Del
@@ -10,6 +10,7 @@ import java.sql.Connection;
 
 public class App
 {
+
     public static void main(String[] args)
     {
 
@@ -22,8 +23,37 @@ public class App
         // Gets connection value
         Connection con = dbc.getConnection();
 
+        // Get population of the world
+        Long totalPopulation = getWorldPopulation(con);
+        System.out.println("World Population: " + totalPopulation);
+
         // Disconnect from database
         dbc.disconnect();
 
+    }
+
+    private static Long getWorldPopulation(Connection con) {
+        try {
+            // Create SQL statement
+            Statement populationStatement = con.createStatement();
+            // Create string for SQL statement
+            String worldPopulation = "SELECT SUM(population) as worldPopulation " +
+                    "FROM country;";
+            // Execute SQL statement
+            ResultSet rset = populationStatement.executeQuery(worldPopulation);
+            // Return new PopulationData if valid
+            // Check something is returned
+            if(rset.next())
+            {
+                Long totalWorldPopulation = rset.getLong("worldPopulation");
+                return totalWorldPopulation;
+            } else
+                return null;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to calculate the population of the world.");
+            return null;
+        }
     }
 }
