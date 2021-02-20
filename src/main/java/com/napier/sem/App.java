ff86d3309fb5577e2aafaec5d6d9c55c94e38ec7
Cameron Hunt <cameronhunt88@hotmail.com>
2021-02-20 21:11:11 +0000

    Implemented a method to return the population of a district + cleanup

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,10 +26,12 @@ public class App
         // Gets connection value
         Connection con = dbc.getConnection();
 
+        // Report 1:
         // Get population of the world
         Long totalPopulation = getWorldPopulation(con);
         System.out.println("World Population: " + totalPopulation);
 
+        // Report 2:
         // Get population of given continent
         // Set continent to get population of
         // 'Asia','Europe','North America','Africa','Oceania','Antarctica','South America'
@@ -37,17 +39,26 @@ public class App
         Long continentPopulation = getPopulationOfContinent(con, continent);
         System.out.println("Population of " + continent + " is " + continentPopulation);
 
+        // Report 3:
         // Get population of given region
         // Set region to get population of
         String region = "Southern Europe";
         Long regionPopulation = getPopulationOfRegion(con, region);
         System.out.println("Population of " + region + " is " + regionPopulation);
 
+        // Report 4:
         // Get the population of given country
         // Set the country code
         String code = "GBR";
         Long countryPopulation = getCountryPopulation(con, code);
         System.out.println("The population of " + countryName + " is " + countryPopulation);
+
+        // Report 5:
+        // Get the population of given district
+        // Set the district
+        String district = "England";
+        Long districtPopulation = getDistrictPopulation(con, district);
+        System.out.println("The population of " + district + " is " + districtPopulation);
 
         // Disconnect from database
         dbc.disconnect();
@@ -55,7 +66,7 @@ public class App
     }
 
     /**
-     * Method to calculate the population of the world
+     * Method to return the population of the world
      * @param con Holds the connection to the SQL server
      * @return The population of the world or null if there is an error
      */
@@ -83,7 +94,7 @@ public class App
     }
 
     /**
-     * Method to calculate the population of a given continent
+     * Method to return the population of a given continent
      * @param con Holds the connection to the SQL server
      * @param continent Contains the continent to get the population for
      * @return The population of the continent or null if there is an error
@@ -112,7 +123,7 @@ public class App
     }
 
     /**
-     * Method to calculate the population of a specified region
+     * Method to return the population of a specified region
      * @param con Holds the connection to the SQL server
      * @param region Contains the region to get the population for
      * @return The population of the region or null if there is an error
@@ -141,7 +152,7 @@ public class App
     }
 
     /**
-     * Method to get the population for a specified country
+     * Method to return the population for a specified country
      * @param con Holds the connection to the SQL server
      * @param code Contains the unique country code used to retrieve the correct population
      * @return Country name & population
@@ -165,7 +176,36 @@ public class App
                 return null;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to calculate the population of the country with the code " + code);
+            System.out.println("Failed to get the population of the country with the code " + code);
+            return null;
+        }
+    }
+
+    /**
+     * Method to return the population of a specified district
+     * @param con Holds the connection to the SQL server
+     * @param district Contains the district to find the population for
+     * @return Population of given district
+     */
+    private static Long getDistrictPopulation(Connection con, String district) {
+        try {
+            // Create SQL statement
+            Statement districtPopulationStatement = con.createStatement();
+            // Create string for SQL statement
+            String districtPopulation = "SELECT SUM(population) as districtPopulation " +
+                    "FROM city " +
+                    "WHERE district = '" + district + "';";
+            // Execute SQL statement
+            ResultSet rset = districtPopulationStatement.executeQuery(districtPopulation);
+            // Return population of specified district
+            // Check something is returned
+            if(rset.next()) {
+                return rset.getLong("districtPopulation");
+            } else
+                return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get the population of the district.");
             return null;
         }
     }
